5f1830a6fb2f107c363dd3ca324e21b6c811c634
Ali <alirabbani54@gmail.com>
2024-04-01 00:49:28 +0500

    init repo

---
+++ tap-boom-game/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+   
+      <div className='text-6xl font-extrabold mx-auto my-auto'>
+        HEllo
+       </div>
+   
+  )
+}
+
+export default App
