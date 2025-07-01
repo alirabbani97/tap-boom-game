93c23c7bf8807d844fd2db26cc0437a5351cfb60
Ali <alirabbani54@gmail.com>
2025-07-01 23:14:10 +0500

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,137 @@
-import { useState } from 'react'
-
+import { BsQuestion } from "react-icons/bs";
 
 function App() {
-
-
   return (
-   
-      <div className='text-6xl font-extrabold mx-auto my-auto'>
-        HEllo
-       </div>
-   
-  )
+    <div className="relative w-full h-screen flex justify-center items-center bg-gradient-to-r from-teal-500 via-cyan-600 to-blue-700">
+      <div className=" grid grid-cols-5 w-max gap-2">
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-md p-5 shadow-[3px_3px_0_2px_rgb(80,80,80)]">
+          <span>
+            <BsQuestion className="text-6xl opacity-70" />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/card.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+interface CardProps{
+    
+}
+
+const Card: React.FC<CardProps>= () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Card
